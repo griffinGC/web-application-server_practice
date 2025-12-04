8809032921b6f6545d84bd925692949e259cfe57
griffinGC <griffindouble@gmail.com>
2025-12-04 21:54:07 +0900

    feature-1 - index.html 응답

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_PORT = 8000;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -18,14 +18,14 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
+        // 서버소켓을 생성한다. 8080 -> 8000 수정
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
+                System.out.println("request handler init!");
                 RequestHandler requestHandler = new RequestHandler(connection);
                 requestHandler.start();
             }
